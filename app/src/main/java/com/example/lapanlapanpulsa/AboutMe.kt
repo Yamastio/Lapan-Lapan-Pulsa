9600773383adc16f302f6eb27b29f6d3f7f867c5
Yamastio <mikuchan312314@gmail.com>
2024-10-17 21:32:53 +0700

    Fix Logo & AboutMeUI

--- app/src/main/java/com/example/lapanlapanpulsa/AboutMe.kt
+++ app/src/main/java/com/example/lapanlapanpulsa/AboutMe.kt
@@ -1,21 +1,118 @@
 package com.example.lapanlapanpulsa
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.lapanlapanpulsa.ui.theme.LapanLapanPulsaTheme
 
 @Composable
-fun AboutMe(){
-    Box(modifier = Modifier.fillMaxSize()){
-        Column(modifier = Modifier
+fun AboutMe() {
+    val scrollState = rememberScrollState()
+
+    Column(
+        modifier = Modifier
             .fillMaxSize()
-            .align(Alignment.Center), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
-            Text(text = "AboutMe")
-        }
+            .padding(16.dp)
+            .verticalScroll(scrollState),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        // Gambar Logo
+        Image(
+            painter = painterResource(id = R.drawable.logo),
+            contentDescription = "Logo Lapan Lapan",
+            modifier = Modifier.size(130.dp),
+            contentScale = ContentScale.Fit
+        )
+
+        Spacer(modifier = Modifier.height(60.dp))
+
+        // Nama Perusahaan
+        Text(
+            text = "PT. ALOMOGADA MANDIRI NUSANTARA" ,
+            fontSize = 18.sp,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Deskripsi
+        Text(
+            text = stringResource(R.string.a2),
+            fontSize = 15.sp,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Text(
+            text = stringResource(R.string.a3),
+            fontSize = 15.sp,
+            textAlign = TextAlign.Center
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Alamat
+        Text(
+            text = "Alamat Kantor Layanan:",
+            fontSize = 15.sp,
+            textAlign = TextAlign.Center,
+            fontWeight = FontWeight.Bold
+        )
+        Text(
+            text = stringResource(R.string.a4),
+            fontSize = 15.sp,
+            textAlign = TextAlign.Center,
+        )
+
+
+        Spacer(modifier = Modifier.height(90.dp))
+
+        // Footer
+        Text(
+            text = stringResource(R.string.nama_aboutme),
+            fontSize = 14.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = stringResource(R.string.versi),
+            fontSize = 14.sp,
+            textAlign = TextAlign.Center
+        )
+        Text(
+            text = stringResource(R.string.tahun),
+            fontSize = 14.sp,
+            textAlign = TextAlign.Center
+        )
+    }
+}
+
+//buatkan preview
+@Preview(showBackground = true)
+@Composable
+fun AboutMePreview() {
+    LapanLapanPulsaTheme {
+        AboutMe()
     }
 }
