ff84d9b7a2b2512dd095422ec938eb7666179793
Yamastio <mikuchan312314@gmail.com>
2024-10-17 21:46:40 +0700

    Fix Versi App

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.lapanlapanpulsa"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 4
+        versionName = "4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
